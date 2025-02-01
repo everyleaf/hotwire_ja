2adb52d40d8ff55ab19e2a06771f4298d808d77f
otegami <a.s.takuya1026@gmail.com>
2025-02-01 10:09:43 +0800

    create or update translation pr

--- tasks/diff_task.rb
+++ tasks/diff_task.rb
@@ -1,8 +1,9 @@
 require "pathname"
 require "tempfile"
 
-require_relative "translated_file"
 require_relative "source_file"
+require_relative "translated_file"
+require_relative "translation_pr"
 
 class DiffTask
   include Rake::DSL
@@ -23,7 +24,14 @@ class DiffTask
           translated_file = source_file.find_translated_file(translated_files)
           next unless translated_file
 
-          diff_content = diff(source_file, translated_file)
+          source_latest_commit, diff_content = diff(source_file, translated_file)
+          next unless diff_content
+
+          translation_pr = TranslationPr.new(translation_repository_path,
+                                             translated_file,
+                                             diff_content,
+                                             source_latest_commit)
+          translation_pr.create_or_update
         end
       end
     end
@@ -34,6 +42,7 @@ class DiffTask
   def diff(source_file, translated_file)
     translated_commit = translated_file.front_matter[:commit]
     return unless translated_commit
+    return if translated_commit == source_latest_commit
 
     Tempfile.open("diff.txt") do |tempfile|
       sh("git",
@@ -43,7 +52,7 @@ class DiffTask
          "#{translated_commit}..#{source_latest_commit}",
          source_file.path.to_s,
          {out: tempfile})
-      tempfile.open.read
+      [source_latest_commit, tempfile.open.read]
     end
   end
 
